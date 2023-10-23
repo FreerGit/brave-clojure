c232b21408d9ab8948a8c59f4ff1f3b3c1b835ba
FreerGit <Freer320@student.liu.se>
2023-10-23 19:50:50 +0200

    chore: scaffold

---
+++ test/freergit/brave_clojure_test.clj
@@ -0,0 +1,7 @@
+(ns freergit.brave-clojure-test
+  (:require [clojure.test :refer :all]
+            [freergit.brave-clojure :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
